86806c68d0dc0be55cc4b8b47147aee14ee1f309
devandtravel <devandtravel@gmail.com>
2021-09-09 14:02:08 +0300

    add: counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ export default function App() {
   const cash = useSelector(state => state.cash)
   console.log(cash)
 
+  const addCash = cash => {
+    dispatch({ type: 'ADD_CASH', payload: cash })
+  }
+  const getCash = cash => {
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
+
   return (
     <div className='App'>
       <div style={{ fontSize: '3rem' }}>{cash}</div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button>Add Cash</button>
-        <button>Get Cash</button>
+        <button onClick={() => addCash(5)}>Add Cash</button>
+        <button onClick={() => getCash(10)}>Get Cash</button>
       </div>
     </div>
   )
